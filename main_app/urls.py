b0be9d1c2312c60cca745bc695a1b71d821bbd9b
mhsmith321 <marty.smith01@yahoo.com>
2021-05-11 12:43:30 -0400

    render quizzes index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('quizzes/', views.quizzes_index, name='quizzes')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def quizzes_index(request):
+    return render(request, 'quizzes/index.html')
