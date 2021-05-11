b0cda977bb43807594f54ae36e22abcf388a0b07
mhsmith321 <marty.smith01@yahoo.com>
2021-05-11 12:12:49 -0400

    render view function home

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Define the home view
+def home(request):
+    return HttpResponse('<h1>This is the view function "home"</h1>')
