66fa67f4defbce3fe151a8c2f77d343acb1d64e3
mhsmith321 <marty.smith01@yahoo.com>
2021-05-13 15:25:58 -0400

    organize admin.py and register Quiz data model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,10 @@
+## IMPORT STATEMENTS  ##
+
+# import admin function (boilerplate feature)
 from django.contrib import admin
 
-# Register your models here.
+# import data models to access in admin portal
+from .models import Quiz
+
+## REGISTER DATA MODELS  ##
+admin.site.register(Quiz)
