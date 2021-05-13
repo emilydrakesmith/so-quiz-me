93b0b3fc46cca9c7340fe2f7dda80e01e2230ca4
mhsmith321 <marty.smith01@yahoo.com>
2021-05-13 15:23:15 -0400

    organize views.py functions

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,30 @@
+## IMPORT STATEMENTS  ##
+
+# import render function
 from django.shortcuts import render
 
-# Define the home view
+# import list functionality for data models
+from django.views.generic import ListView
+
+# import data models for class-based views
+from .models import Quiz
+
+##  RENDER VIEW FUNCTIONS  ##
+
+# view function for route '/'
 def home(request):
     return render(request, 'home.html')
 
+# view funciton for route '/about'
 def about(request):
     return render(request, 'about.html')
 
+#view function for route '/quizzes'
 def quizzes_index(request):
     return render(request, 'quizzes/index.html')
+
+##  CLASS-BASED VIEW FUNCTIONS  ##
+
+# class-based view for quizzes
+class QuizList(ListView):
+    model = Quiz
