3d29c6969c1380e02b436da718eb68365ccbf548
mhsmith321 <marty.smith01@yahoo.com>
2021-05-13 23:12:59 -0400

    expand model Quiz

--- main_app/models.py
+++ main_app/models.py
@@ -8,6 +8,15 @@ from django.urls import reverse     # allows reverse-redirect on form submission
 class Quiz(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField(max_length=300)
+    question_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # TODO: add a slug for URL path generation (icebox)
+
+    class Meta:
+        verbose_name = 'Quiz'               # singular name in admin portal
+        verbose_name_plural = 'Quizzes'     # plural name in admin portal
+        # TODO: add 'ordering' for sequencing on page when rendered
 
     def __str__(self):
         return self.title    # return title instead of id when model is called
